dab8c66a7c398ccfa10cd6813ea16f25db4acaef
andrewome <andrewong3012@gmail.com>
2018-06-29 14:40:59 +0800

    start

--- New Text Document.bat
+++
@@ -1,6 +0,0 @@
-echo "# PartyCards" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/andrewome/PartyCards.git
-git push -u origin master
