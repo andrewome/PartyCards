f6d94c4d73ab7591ee9bfbdc302c2b7c98f32768
sxqnicholas96 <37098263+sxqnicholas96@users.noreply.github.com>
2018-06-29 22:28:54 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,52 +1,46 @@
-var express = require('express');
-var socket = require('socket.io');
-var portNum = 1520;
-var connected_ids = [];
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import './homepage.css';
+import registerServiceWorker from './registerServiceWorker';
 
-// function to print all connected IDs that are in the server now
-function printConnectedIDs() {
-	var i, size = connected_ids.length;
-	console.log("Currently there are " + size + " user(s) in your server.");
-	for(i=0;i<size;i++) {
-		console.log(i+1 + ". ID: " + connected_ids[i]);
-	}
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <h1>Welcome to PartyCards!</h1>
+      </div>
+    );
+  }
+}
+class Body extends Component {
+  constructor() {
+    super();
+    this._handleClick = this._handleClick.bind(this);
+  }
+  state = {
+    server_PIN: "Enter PIN here"
+  }
+  _handleClick() {
+    let mountNode = React.findDOMNode(this.refs.Init);
+    let unmount = React.unmountComponentAtNode(mountNode);
+    // console.log(unmount); // false
+  }
+
+  render() {
+    return (
+      <div className="Init">
+        <button type = "primary" className = "server" onClick = {this._handleClick}> Create New Server </button>
+        <br/>
+        <p className = "para"> or </p>
+        <input type = "text" className = "textfield" value = {this.state.server_PIN} onChange={(e) => {this.setState({server_PIN: e.target.value})}}></input>
+        <br/>
+        <button className = "server">Join Existing Server</button>
+      </div>
+    );
+  }
 }
 
-// function to delete a certain id from our list of ids array
-function delete_id(id) {
-	var i, size = connected_ids.length;
-	for(i=0;i<size;i++) {
-		if(connected_ids[i] == id) {
-			break;
-		}
-	}
-	connected_ids.splice(i, 1);
-}
-
-// App setup
-var app = express();
-var server = app.listen(portNum, function() { 
-	console.log('Server has been started. Port number = ' + portNum);
-});
-
-// Set up standard file
-app.use(express.static('public'));
-
-// Socket setup
-var io = socket(server);
-
-
-// When connected, output message onto server side console, add to list of connected clients
-io.on('connection', function(socket) {
-	console.log(socket.id + " has made socket connection to server");
-	connected_ids.push(socket.id);
-	printConnectedIDs();
-	
-	// When disconnected, output message to server side console, and remove from list of connected clients
-	socket.on('disconnect', function() {
-		console.log(socket.id + " has disconnected from the server");
-		delete_id(socket.id);
-		printConnectedIDs();
-	});
-});
-
+ReactDOM.render(<App />, document.getElementById('title'));
+ReactDOM.render(<Body />, document.getElementById('body'));
+registerServiceWorker();
