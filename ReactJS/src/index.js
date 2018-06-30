d4aab5d79656e5067bd6cdaf7b0f37178bf31eea
andrewome <andrewong3012@gmail.com>
2018-06-30 16:12:52 +0800

    added some backend

--- ReactJS/src/index.js
+++ ReactJS/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import './homepage.css';
 import App from './App.js';
 import registerServiceWorker from './registerServiceWorker';
+import './socketio.js';
 
 
 ReactDOM.render(<App />, document.getElementById('body'));
