61dc71f8409305db101645f1c344cfc3c7e516c7
sxqnicholas96 <sxq.nicholas96@gmail.com>
2018-07-01 23:24:48 +0800

    Looking Good

--- ReactJS/src/Games/Cheat.js
+++ ReactJS/src/Games/Cheat.js
@@ -13,8 +13,9 @@ class Taiti extends Component {
 		instance: {},
 		server_PIN: this.props.serverPIN,
 		top_deck: "Cheat!",
-		message: "Select a card!",
+		message: "Starting game...",
 		selected_cards: [],
+		declared_cards: [],
 		Discard_pile: [],
 		playerID: "",
 		player_hand: [],
@@ -40,25 +41,54 @@ class Taiti extends Component {
 
 		}.bind(this));
 	}
-	handleSelect_Card = (name) => {
-		this.setState({message: "You selected: " + name});
+	handleSelect_Card = (card) => {
+		this.setState({message: "You selected: " + card.name});
+	}
+	handleSubmit(e){
+    if(this.refs.num.value === ''){
+      alert('Number of cards not entered!');
+    }
+		if(this.refs.val.value === ''){
+			alert('Value of cards not entered!');
+		}
+    else{
+      this.setState({declared_cards: [parseInt(this.refs.num.value),parseInt(this.refs.val.value)]}
+			);
+      }
+    //console.log(this.refs.title.value);
+     e.preventDefault();
+  }
+	handleCheat = () =>{
+
 	}
 
 	render() {
 		var playerhand = this.state.player_hand
     //Sorts hand according to value
-    //Sort.byValue(playerhand);
-    var selected_cards = []
+    Sort.byValue(playerhand);
+    var selectedcards = this.state.selected_cards
     const listHand = playerhand.map((d) =>
       <button className = "Cards" onClick = {() => {
-        this.handleSelect_Card(d.name)
-        selected_cards.push(d.name)
+				let index = playerhand.findIndex(x => x.name === d.name)
+				playerhand.splice(index,1)
+				this.setState({player_hand: playerhand})
+				selectedcards.push(d)
+        this.handleSelect_Card(d)
+				this.setState({selected_cards: selectedcards})
       }}
       key={d.name}>{d.name}
       </button>);
-    const listCards = selected_cards.map((card) =>
-      <li key = {card.name}>{card.name}
-      </li>);
+    const listCards = this.state.selected_cards.map((d) =>
+		<lu className = "Cards"
+		key={d.name}>{d.name} <a href='#' onClick = {() =>{
+			let index = selectedcards.findIndex(x => x.name === d.name)
+			selectedcards.splice(index,1)
+			this.setState({selected_cards: selectedcards})
+			playerhand.push(d	)
+			this.setState({player_hand: playerhand})
+
+		}}>X</a> </lu>);
+
     return(
       <div className = "Parent">
 				<p> {this.state.instance.pinNo} </p>
@@ -66,10 +96,19 @@ class Taiti extends Component {
 				/>
 				<div className = "p1">
 					{listHand}
-					<button className = "button">Call Bluff!</button>
+					<form onSubmit = {this.handleSubmit.bind(this)}>
+						<label>Enter number of card(s): </label>
+						<input type = "text" className = "field" ref = "num"/>
+						<label> Enter value of card(s): </label>
+						<input type="text" className = "field" ref = "val"/>
+						<input type = "submit" value = "Play Cards!" className = "button"/>
+					</form>
+					<button className = "button" onClick = {this.handCheat
+
+					}>Call Cheat!</button>
 				</div>
 				<p className = "p1">{this.state.message}</p>
-				{listCards}
+					{listCards}
 				<h1 className = "discardpile">{this.state.top_deck}</h1>
       </div>
     );
