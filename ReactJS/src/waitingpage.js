39354207ba61113c65a941f4db2f1c3c6e5e2316
sxqnicholas96 <sxq.nicholas96@gmail.com>
2018-07-01 21:35:22 +0800

    waiting page update

--- ReactJS/src/waitingpage.js
+++ ReactJS/src/waitingpage.js
@@ -5,6 +5,10 @@ class WaitingPage extends Component{
   constructor(){
     super(props);
   }
+  return(){
+    <h3>Room pin: </h3>
+    <p>Waiting for players....</p>
+  }
 }
 
 export default WaitingPage;
